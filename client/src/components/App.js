cab3a69f6d0e02162c184ed546c3b55421f6f8da
timTonelli <102175985+timTonelli@users.noreply.github.com>
2022-05-16 11:52:16 -0400

    add basic styling and spacing to nav bar

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -28,7 +28,7 @@ const App = (props) => {
       <TopBar user={currentUser} />
       <Switch>
         <Route exact path="/">
-          <h2>Hello from react</h2>
+          <h2 className="text-4xl">Hello from react</h2>
         </Route>
         <Route exact path="/users/new" component={RegistrationForm} />
         <Route exact path="/user-sessions/new" component={SignInForm} />
